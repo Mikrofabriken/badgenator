38beedbb7efd8db5e4c1ed24b3697f2f8390ee1a
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-05-19 16:35:10 +0200

    add text only mode

---
+++ src/routes/text-only/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import TextOnlyBadge from './TextOnlyBadge.svelte';
+	import { download } from '../helpers';
+
+	var name = '';
+	const badgeIconsText = '';
+
+	function handleSubmit() {
+		download(name, badgeIconsText);
+	}
+</script>
+
+<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+	<div class="sm:mx-auto sm:w-full sm:max-w-md">
+		<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+			Mikrofabriken Badgenator
+		</h2>
+	</div>
+
+	<div class="mt-6 sm:mx-auto sm:w-full sm:max-w-xl">
+		<div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+			<form class="space-y-6" on:submit|preventDefault={handleSubmit}>
+				<div>
+					<label for="name" class="block text-sm font-medium leading-6 text-gray-900"> Namn </label>
+					<div class="mt-2">
+						<input
+							id="name"
+							name="name"
+							type="text"
+							autocomplete="off"
+							required
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							bind:value={name}
+						/>
+					</div>
+				</div>
+
+				<div class="space-y-2">
+					<div class="flex justify-center py-4">
+						<TextOnlyBadge {name} />
+					</div>
+
+					<div>
+						<button
+							type="submit"
+							class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						>
+							Ladda ner .svg av din namnbricka!
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
---
+++ src/routes/text-only/TextOnlyBadge.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let name = '';
+</script>
+
+<svg
+	version="1.1"
+	id="Layer_1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	x="0px"
+	y="0px"
+	width="80mm"
+	height="25mm"
+	viewBox="0 0 226.77 70.87"
+	style="enable-background:new 0 0 226.77 70.87;"
+	xml:space="preserve"
+>
+	<style type="text/css">
+		@font-face {
+			font-family: IndustryInc;
+			src: url('IndustryInc.ttf');
+		}
+
+		.badge_name {
+			font-family: 'IndustryInc';
+			font-size: 23.4px;
+		}
+	</style>
+	<text
+		id="badge_name"
+		text-anchor="middle"
+		transform="matrix(1 0 0 1 143.9001 43.4193)"
+		class="badge_name"
+	>
+		{name}
+	</text>
+</svg>
