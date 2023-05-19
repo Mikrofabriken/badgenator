d98926b22ba5c98066b2a556fd0bc4fce36980d1
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-05-19 16:40:35 +0200

    add background to text-only mode

--- src/routes/text-only/TextOnlyBadge.svelte
+++ src/routes/text-only/TextOnlyBadge.svelte
@@ -21,11 +21,22 @@
 			src: url('IndustryInc.ttf');
 		}
 
+		.yttre {
+			fill: #d1d3d4;
+		}
+
 		.badge_name {
 			font-family: 'IndustryInc';
 			font-size: 23.4px;
 		}
 	</style>
+	<g id="YTTRE">
+		<path
+			class="yttre"
+			d="M191.34,0H35.43C15.86,0,0,15.86,0,35.43v0C0,55,15.86,70.87,35.43,70.87h155.91
+      c19.57,0,35.43-15.86,35.43-35.43v0C226.77,15.86,210.91,0,191.34,0z"
+		/>
+	</g>
 	<text
 		id="badge_name"
 		text-anchor="middle"
